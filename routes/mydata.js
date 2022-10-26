6c68b2d7d952f825a571abcddb78f8d24d9f073a
Sravan Satish Jasti <sravansatish2000@gmail.com>
2022-10-26 18:19:46 -0500

    added the new end point

---
+++ routes/mydata.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'Sravan Satish Jasti' });
+});
+module.exports = router;
